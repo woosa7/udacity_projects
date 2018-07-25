5c82bcb37cd9acc91e64365612b560643c6d2529
woosa7 <woosa7@gmail.com>
2018-07-25 16:34:52 +0900

    decision tree

---
+++ ML_DL_Balazs/e10_decision_tree.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split, cross_val_predict
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn import datasets
+
+dataset = datasets.load_iris()      # count = 150
+
+dataset.features = dataset.data
+dataset.targets = dataset.target
+
+feature_train, feature_test, target_train, target_test = train_test_split(dataset.features, dataset.targets, test_size=.3)
+
+model = DecisionTreeClassifier(criterion='gini')
+model.fitted = model.fit(feature_train, target_train)
+model.predictions = model.fitted.predict(feature_test)
+
+print(confusion_matrix(target_test, model.predictions))
+print(accuracy_score(target_test, model.predictions))
+
+predicted = cross_val_predict(model, dataset.features, dataset.targets, cv=10)
+print(accuracy_score(dataset.targets, predicted))
